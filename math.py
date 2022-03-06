3742497e11f551fedad989fd4d358655e2c6be16
Avinash Ahire <avinash_ahire@persistent.com>
2022-03-06 20:09:40 +0530

    changes in add implementation function

---
+++ math.py
@@ -0,0 +1,20 @@
+#add implementation
+def add(x,y):
+     return x+y
+    
+ 
+#subtract implementation 
+def subtract(x,y):
+    pass    
+    
+    #multi implementation
+def multiply(x,y):
+    pass
+    
+    #div implementation
+def divide(x,y):
+   pass 
+   
+
+    
+    
