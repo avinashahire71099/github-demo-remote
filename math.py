42b701d595a98e80276e7390d2480cc410c3f907
avinashahire71099 <88340119+avinashahire71099@users.noreply.github.com>
2022-03-06 20:01:56 +0530

    New Math File Added

---
+++ math.py
@@ -0,0 +1,20 @@
+#add implementation
+def add(x,y):
+     pass
+    
+ 
+#subtract implementation 
+def subtract(x,y):
+    pass    
+    
+    #multi implementation
+def multiply(x,y):
+    pass
+    
+    #div implementation
+def divide(x,y):
+   pass 
+   
+
+    
+    
