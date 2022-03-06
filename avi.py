0ce1bfc64cb5c54c96a0b6031da461e801b16654
Avinash Ahire <avinash_ahire@persistent.com>
2022-03-06 12:08:49 +0530

    Added function header

--- avi.py
+++ avi.py
@@ -1,12 +1,16 @@
+#add implementation
 def add(x,y):
     return x+y
-    
+ 
+#subtract implementation 
 def subtract(x,y):
     return x-y       #on master branch
     
+    #multi implementation
 def multiply(x,y):
     return x*y  #implemented bug 456 multipy function
     
+    #div implementation
 def divide(x,y):
     if y==0:             #Implementation for master branch
         return DIVIDE_BY_0_ERROR
